2305c025e4f814d915d9550d6d48abdb54381683
Anna <annamtoman@gmail.com>
2023-03-22 14:23:43 -0500

    seeds users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user.create(name: "Mike", email: "mike@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/52894341?v=4")
+
+user.create(name: "Ed", email: "Ed@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/97701763?v=4")
+
+user.create(name: "Josh", email: "josh@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/125905920?v=4")
+
+user.create(name: "Anna", email: "anna@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/121132842?v=4")
