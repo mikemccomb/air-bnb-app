2203f51298f3b696196136346e50d7849f276aad
joshua <joshuamtaylor455@gmail.com>
2023-03-22 16:32:38 -0400

    fix errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(
-    name: params[:user][:name]
-    email: params[:user][:email]
-    password: params[:user][:password]
-    password_confirmation: params[:user][:password_confirmation]
+      name: params[:user][:name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation],
     )
     if @user.save
       session[:user_id] = @user.id
       redirect_to "/"
-    else  
+    else
       render :new, status: :unprocessable_entity
     end
   end
-
-  
 end
