ec1938551de7ab78f9b41b33c6b5160197c3c7da
Anna <annamtoman@gmail.com>
2023-03-22 15:10:28 -0500

    adds user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-user.create(name: "Mike", email: "mike@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/52894341?v=4")
+user = User.new(name: "Mike", email: "mike@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/52894341?v=4")
+user.save
 
-user.create(name: "Ed", email: "Ed@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/97701763?v=4")
+user = User.new(name: "Ed", email: "Ed@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/97701763?v=4")
+user.save
 
-user.create(name: "Josh", email: "josh@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/125905920?v=4")
+user = User.new(name: "Josh", email: "josh@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/125905920?v=4")
+user.save
 
-user.create(name: "Anna", email: "anna@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/121132842?v=4")
+user = User.new(name: "Anna", email: "anna@test.com", password_digest: "password", image: "https://avatars.githubusercontent.com/u/121132842?v=4")
+user.save
