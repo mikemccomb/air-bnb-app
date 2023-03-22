9ddb132d20c8ad5ce96c12fac602a1fc2296682f
joshua <joshuamtaylor455@gmail.com>
2023-03-22 16:16:53 -0400

    add login, create session, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # login routes
   get "/signup" => "users#new"
   post "/users" => "users#create"
+
+  # sessions routes
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
