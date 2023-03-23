fde2be10fb37826773949bd6a9591510dc2e785b
Anna <annamtoman@gmail.com>
2023-03-23 15:34:37 -0500

    adds review new html

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,26 @@
 class ReviewsController < ApplicationController
 
+    def index
+        @reviews = Review.all
+        render :index
+    end
+
     def new
         @review = Review.new
-        render :new
+        render template: "reviews/new"
     end
 
     def create
-        @review = Review.create(
-          reservation_id: params[:reservations][:reservations_id],
+        @review = Review.new(
+          reservation_id: params[:reservation_id],
           rating: params[:rating],
           comment: params[:comment]
         )
-        render :show
+        if @review.save
+            redirect_to "/rooms"
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
 end
