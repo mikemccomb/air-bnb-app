5f55d2977ca7de3d816780c124ec556ab8151975
“Edward <eminaya49@gmail.com>
2023-03-24 12:15:09 -0600

    updated index view with bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@ user.save
 user = User.new(name: "Anna", email: "anna@test.com", password: "password", password_confirmation: "password", image: "https://avatars.githubusercontent.com/u/121132842?v=4")
 user.save
 
-room = Room.new(user_id: 1, address: "123 Hollywood Blvd", city: "LA", state: "CA", price: 300, description: "Come stay at Jason Nesmith's home set in the idyllic Hollywood Hills. Watch out for foreign invaders", home_type: "Mansion Home", room_type: "Private", total_occupancy: 12, total_bedrooms: 8, total_bathrooms: 4)
+room = Room.new(user_id: 1, address: "123 Hollywood Blvd", city: "Los Angeles", state: "California", price: 300, description: "Come stay at Jason Nesmith's home set in the idyllic Hollywood Hills. Watch out for foreign invaders", home_type: "Mansion Home", room_type: "Private", total_occupancy: 12, total_bedrooms: 8, total_bathrooms: 4)
 room.save
 
-room = Room.new(user_id: 2, address: "190 W Sixth Street", city: "Boston", state: "MA", price: 100, description: "Have you ever met a Will Hunting you didn't like? Now you have the chance to stay in his three flat! Located right in the heart of Boston", home_type: "Apartment", room_type: "Shared", total_occupancy: 3, total_bedrooms: 2, total_bathrooms: 1)
+room = Room.new(user_id: 2, address: "190 W Sixth Street", city: "Boston", state: "Massachusetts", price: 100, description: "Have you ever met a Will Hunting you didn't like? Now you have the chance to stay in his three flat! Located right in the heart of Boston", home_type: "Apartment", room_type: "Shared", total_occupancy: 3, total_bedrooms: 2, total_bathrooms: 1)
 room.save
 
-room = Room.new(user_id: 3, address: "6409 Wilbur Drive", city: "Austin", state: "TX", price: 200, description: "You won't be confused, but you will be dazed at Carl's cool house. Located on the beautiful streets of Austin!", home_type: "Single-Family Home", room_type: "Private", total_occupancy: 12, total_bedrooms: 4, total_bathrooms: 3)
+room = Room.new(user_id: 3, address: "6409 Wilbur Drive", city: "Austin", state: "Texas", price: 200, description: "You won't be confused, but you will be dazed at Carl's cool house. Located on the beautiful streets of Austin!", home_type: "Single-Family Home", room_type: "Private", total_occupancy: 12, total_bedrooms: 4, total_bathrooms: 3)
 room.save
 
-room = Room.new(user_id: 4, address: "122 Callahan Drive", city: "Sandusky", state: "OH", price: 250, description: "Want to live in the coolest house in the middle of nowhere? Come visit Callahan Auto Parts and hangout with Tomy", home_type: "Mansion", room_type: "Private", total_occupancy: 12, total_bedrooms: 8, total_bathrooms: 6)
+room = Room.new(user_id: 4, address: "122 Callahan Drive", city: "Sandusky", state: "Ohio", price: 250, description: "Want to live in the coolest house in the middle of nowhere? Come visit Callahan Auto Parts and hangout with Tomy", home_type: "Mansion", room_type: "Private", total_occupancy: 12, total_bedrooms: 8, total_bathrooms: 6)
 room.save
 
 roomimage = RoomImage.new(room_id: 1, url: "https://upload.wikimedia.org/wikipedia/commons/2/22/Case_Study_House_No._22.JPG")
@@ -49,4 +49,3 @@ review = Review.create(reservation_id: 2, rating: 3, comment: "Pretty okay.")
 review = Review.create(reservation_id: 3, rating: 1, comment: "It was good for what we wanted.")
 
 review = Review.create(reservation_id: 4, rating: 4, comment: "Classic, I love Sandusky, OH.")
-
